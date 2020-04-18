4dad04ef615f22d7d59e72fa2b7b6657f0d7a036
Dmytro Anikin <dimianni@users.noreply.github.com>
2020-04-18 21:33:21 +0200

    Updates

---
+++ precache-manifest.fe8299c34db1729e8c3bc9eebfaa7d4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fcd41fa7a2809127db367fe2c105f6",
+    "url": "/dimianni-facedetector/index.html"
+  },
+  {
+    "revision": "367992376d21e7767e4d",
+    "url": "/dimianni-facedetector/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "62614f6090772c51389d",
+    "url": "/dimianni-facedetector/static/css/main.b3925fbb.chunk.css"
+  },
+  {
+    "revision": "367992376d21e7767e4d",
+    "url": "/dimianni-facedetector/static/js/2.ba4efc15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dimianni-facedetector/static/js/2.ba4efc15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62614f6090772c51389d",
+    "url": "/dimianni-facedetector/static/js/main.e01a4272.chunk.js"
+  },
+  {
+    "revision": "7bdd1092aae38cc8d208",
+    "url": "/dimianni-facedetector/static/js/runtime-main.c1580328.js"
+  },
+  {
+    "revision": "8fe6c552d284717ca050825d652219cf",
+    "url": "/dimianni-facedetector/static/media/Dimianni.8fe6c552.svg"
+  }
+]);
